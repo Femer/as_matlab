b057f1fe40f58b7a8317c3814d98bdc0ffba4d78
Femer <femer90@hotmail.it>
2015-02-28 17:48:55 +0100

    linkex axes in plot PSD

--- SysId/StateSpace/GUI_online_id/tool/tool_plotPSD.m
+++ SysId/StateSpace/GUI_online_id/tool/tool_plotPSD.m
@@ -6,7 +6,7 @@ avgFs = tool_computeAvgSamplingF(logStr.time);
 %plot yawRate nad yaw PSD
 figure;
 
-subplot(211);
+h1 = subplot(211);
 [psdYR, f] = pwelch(logStr.yawRate, [], [], [], avgFs);
 plot(f, psdYR, 'LineWidth', 1.7);
 grid on;
@@ -14,7 +14,7 @@ xlabel('Frequency [Hz]');
 ylabel('Magnitude [dB]');
 title('YawRate');
 
-subplot(212);
+h2 = subplot(212);
 [psdY, f] = pwelch(logStr.yaw, [], [], [], avgFs);
 plot(f, psdY, 'LineWidth', 1.7);
 grid on;
@@ -22,6 +22,8 @@ xlabel('Frequency [Hz]');
 ylabel('Magnitude [dB]');
 title('YawRate');
 
-
+%link axes when zoom or move
+linkaxes([h1, h2], 'x');
+%xlim(h1, [time_sec(1) time_sec(end)]);
 end
 
